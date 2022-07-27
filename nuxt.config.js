b624b2069f4ec3baa4380832487cef67ed1783a0
Amice13 <kirillzakharov13@gmail.com>
2022-07-27 20:57:04 +0300

    no hope

--- nuxt.config.js
+++ nuxt.config.js
@@ -113,13 +113,4 @@ export default {
   generate: {
     dir: 'docs'
   },
-  build: {
-    extend (config, { isDev, isClient }) {
-      if (!isDev) {
-        // relative links, please.
-        config.output.publicPath = './_nuxt/'
-      }
-      return config;
-    }
-  }
 }
