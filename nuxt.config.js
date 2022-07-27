b0b4031afc7d286b7e356b18230278769a48a3cb
Amice13 <kirillzakharov13@gmail.com>
2022-07-27 20:42:09 +0300

    a

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,6 +6,9 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/peremoha/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
